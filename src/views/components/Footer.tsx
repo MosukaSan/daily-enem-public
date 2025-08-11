e2209268b972f3efdda9b93e1b32157b7707d4d6
Lucas Lima <mosukasangamer@gmail.com>
2025-08-11 14:03:11 -0300

    Fix footer wider than screen on mobile

--- src/views/components/Footer.tsx
+++ src/views/components/Footer.tsx
@@ -6,14 +6,14 @@ import type { JSX } from "react";
 function Footer(): JSX.Element {
     return (
         <footer className="border-t-3 border-dark-highlight">
-            <div className="container mx-auto flex justify-between items-center py-3 px-3 text-text">
-                <div>
+            <div className="container mx-auto flex justify-between items-center py-3 px-6 text-text gap-3">
+                <div className="flex-shrink-0">
                     <a href="https://github.com/MosukaSan/daily-enem-public" className="hover:text-text-hover text-2xl"><FontAwesomeIcon icon={faGithub} /></a>
                 </div>
-                <div>
+                <div className="text-justify p-3">
                     <p>Algumas questões podem precisar da questão anterior.</p>
                 </div>
-                <div onClick={() => window.location.href = "https://enem.dev/"} className="group flex gap-3 items-center hover:cursor-pointer hover:text-text-hover">
+                <div onClick={() => window.location.href = "https://enem.dev/"} className="group flex gap-3 items-center hover:cursor-pointer hover:text-text-hover flex-shrink-0">
                     <h3 className="font-bold">API<br />Enem</h3>
                     <img src={apiEnemIcon} alt="api enem image" className="h-9 group-hover:opacity-80" />
                 </div>
